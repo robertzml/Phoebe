b59b18cecf2cdd38820d013bc0a2d9bb8df55c47
robertzml <robertzml@126.com>
2015-08-06 16:03:53 +0800

    use theme metronic 4.1.
    Signed-off-by: robertzml <robertzml@126.com>

--- Phoebe.UI/App_Start/BundleConfig.cs
+++ Phoebe.UI/App_Start/BundleConfig.cs
@@ -14,6 +14,18 @@ namespace Phoebe.UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                        "~/assets/global/plugins/jquery.min.js",
+                        "~/assets/global/plugins/jquery-migrate.min.js",
+                        "~/assets/global/plugins/jquery-ui/jquery-ui.min.js",
+                        "~/assets/global/plugins/bootstrap/js/bootstrap.min.js",
+                        "~/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js",
+                        "~/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                        "~/assets/global/plugins/jquery.cokie.min.js",
+                        "~/assets/global/plugins/uniform/jquery.uniform.min.js"));
+
+
+
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
