920e968f534f7a7b973003683393a7faf8ef2dac
robertzml <robertzml@126.com>
2015-08-15 20:51:38 +0800

    Add category index view.
    Signed-off-by: robertzml <robertzml@126.com>

--- Phoebe.Business/CategoryBusiness.cs
+++ Phoebe.Business/CategoryBusiness.cs
@@ -22,5 +22,45 @@ namespace Phoebe.Business
             this.context = new PhoebeContext();
         }
         #endregion //Constructor
+
+        #region Method
+        /// <summary>
+        /// 获取所有一级分类
+        /// </summary>
+        /// <returns></returns>
+        public List<FirstCategory> GetFirstCategory()
+        {
+            return this.context.FirstCategories.ToList();
+        }
+
+        /// <summary>
+        /// 获取一级分类
+        /// </summary>
+        /// <param name="id">一级分类ID</param>
+        /// <returns></returns>
+        public FirstCategory GetFirstCategory(int id)
+        {
+            return this.context.FirstCategories.SingleOrDefault(r => r.ID == id);
+        }
+
+        /// <summary>
+        /// 获取所有二级分类
+        /// </summary>
+        /// <returns></returns>
+        public List<SecondCategory> GetSecondCategory()
+        {
+            return this.context.SecondCategories.ToList();
+        }
+
+        /// <summary>
+        /// 获取二级分类
+        /// </summary>
+        /// <param name="id">二级分类ID</param>
+        /// <returns></returns>
+        public SecondCategory GetSecondCategory(int id)
+        {
+            return this.context.SecondCategories.SingleOrDefault(r => r.ID == id);
+        }
+        #endregion //Method
     }
 }
---
+++ Phoebe.UI/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Phoebe.Business;
+using Phoebe.Common;
+using Phoebe.Model;
+
+namespace Phoebe.UI.Controllers
+{
+    /// <summary>
+    /// 类别控制器
+    /// </summary>
+    public class CategoryController : Controller
+    {
+        #region Field
+        /// <summary>
+        /// 类别业务
+        /// </summary>
+        private CategoryBusiness categoryBusiness;
+        #endregion //Field
+
+        #region Constructor
+        public CategoryController()
+        {
+            this.categoryBusiness = new CategoryBusiness();
+        }
+        #endregion ///Constructor
+
+        #region Action
+        /// <summary>
+        /// 类别主页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var data = this.categoryBusiness.GetFirstCategory();
+            return View(data);
+        }
+        #endregion //Action
+    }
+}
