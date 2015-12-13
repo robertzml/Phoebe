0509d9a7dd7f60bc44a35f2e8815ea4fce1fb479
robertzml <robertzml@126.com>
2015-12-13 19:56:29 +0800

    Add Phoebe.FormUI project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.FormUI/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Phoebe.FormUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.menuCustomer = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCustomerList = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCustomer});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(854, 25);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // menuCustomer
+            // 
+            this.menuCustomer.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCustomerList});
+            this.menuCustomer.Name = "menuCustomer";
+            this.menuCustomer.Size = new System.Drawing.Size(68, 21);
+            this.menuCustomer.Text = "客户管理";
+            // 
+            // menuCustomerList
+            // 
+            this.menuCustomerList.Name = "menuCustomerList";
+            this.menuCustomerList.Size = new System.Drawing.Size(152, 22);
+            this.menuCustomerList.Text = "客户列表";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 506);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "MainForm";
+            this.Text = "华润冷链管理系统";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem menuCustomer;
+        private System.Windows.Forms.ToolStripMenuItem menuCustomerList;
+    }
+}
+
