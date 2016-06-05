954a5887bdeb288024a288cfcb2bd2b455d15420
robertzml <robertzml@126.com>
2016-06-05 20:06:46 +0800

    Add global exception handle, not enable.
    Signed-off-by: robertzml <robertzml@126.com>

--- Phoebe.FormClient/Program.cs
+++ Phoebe.FormClient/Program.cs
@@ -27,6 +27,7 @@ namespace Phoebe.FormClient
             DevExpress.Skins.SkinManager.EnableMdiFormSkins();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
 
             LoginForm login = new LoginForm();
             if (login.ShowDialog() == DialogResult.OK)
@@ -35,6 +36,20 @@ namespace Phoebe.FormClient
                 Cache.Instance.Add("CurrentUser", Program.GC.CurrentUser); //缓存用户信息
 
                 Application.Run(new MainForm());
+            }
+        }
+
+        /// <summary>
+        /// 异常消息处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="ex"></param>
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs ex)
+        {
+            string message = string.Format("{0}\r\n操作发生错误，您需要退出系统么？", ex.Exception.Message);
+            if (MessageUtil.ConfirmYesNo(message) == DialogResult.Yes)
+            {
+                Application.Exit();
             }
         }
     }
