c737b7f01401b9bcb9419c7f8d45c163326552f4
robertzml <robertzml@126.com>
2018-08-06 15:22:17 +0800

    Add Phoebe.Core project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.Core/BL/UserBusiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoebe.Core.BL
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Base.System;
+    using Phoebe.Core.IDAL;
+    using Phoebe.Core.DL;
+
+    /// <summary>
+    /// 用户业务类
+    /// </summary>
+    public class UserBusiness : AbstractBusiness<User, int>, IBaseBL<User, int>
+    {
+        #region Constructor
+        /// <summary>
+        /// 用户业务类
+        /// </summary>
+        public UserBusiness()
+        {
+            this.baseDal = RepositoryFactory<IUserRepository>.Instance;
+        }
+        #endregion //Constructor
+    }
+}
---
+++ Phoebe.Core/BL/UserGroupBusiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoebe.Core.BL
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Base.System;
+    using Phoebe.Core.IDAL;
+    using Phoebe.Core.DL;
+
+    /// <summary>
+    /// 用户组业务类
+    /// </summary>
+    public class UserGroupBusiness : AbstractBusiness<UserGroup, int>, IBaseBL<UserGroup, int>
+    {
+        #region Constructor
+        /// <summary>
+        /// 用户组业务类
+        /// </summary>
+        public UserGroupBusiness()
+        {
+            this.baseDal = RepositoryFactory<IUserGroupRepository>.Instance;
+        }
+        #endregion //Constructor
+    }
+}
