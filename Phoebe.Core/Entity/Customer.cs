da8955b368ac8753ac9979d0e5fe6edceada7210
robertzml <robertzml@126.com>
2019-08-11 23:35:27 +0800

    add Phoebe.Core project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.Core/Entity/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using SqlSugar;
+
+namespace Phoebe.Core.Entity
+{
+    /// <summary>
+    /// 客户类
+    /// </summary>
+    [SugarTable("Customer")]
+    public class Customer
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 代码
+        /// </summary>
+        [Display(Name = "代码")]
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        [Display(Name = "地址")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 电话
+        /// </summary>
+        [Display(Name = "电话")]
+        public string Telephone { get; set; }
+
+        /// <summary>
+        /// 联系人
+        /// </summary>
+        [Display(Name = "联系人")]
+        public string Contact { get; set; }
+
+        /// <summary>
+        /// 联系人电话
+        /// </summary>
+        [Display(Name = "联系人电话")]
+        public string ContactTelephone { get; set; }
+
+        /// <summary>
+        /// 客户类型
+        /// </summary>
+        [Display(Name = "客户类型")]
+        public int Type { get; set; }
+        #endregion //Property
+    }
+}
