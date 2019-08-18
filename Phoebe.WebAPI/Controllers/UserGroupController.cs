75bc376e564a617030df699fcef4ad31c7503f54
robertzml <robertzml@126.com>
2019-08-18 23:54:01 +0800

    add UserGroupController.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.WebAPI/Controllers/UserGroupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Phoebe.WebAPI.Controllers
+{
+    using Phoebe.Core.BL;
+    using Phoebe.Core.Entity;
+    using Phoebe.WebAPI.Model;
+
+    /// <summary>
+    /// 用户组控制器
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserGroupController : ControllerBase
+    {
+        #region Action
+        /// <summary>
+        /// 获取所有用户组
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult<List<UserGroup>> List()
+        {
+            UserGroupBusiness userGroupBusiness = new UserGroupBusiness();
+            return userGroupBusiness.FindAll();
+        }
+        #endregion //Action
+    }
+}
