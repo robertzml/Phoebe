70d957558dbb04c8bb7271733c71e4596ffbfb87
robertzml <robertzml@126.com>
2019-09-07 22:53:15 +0800

    add StockIn, StockInTask model.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.Core/Entity/StockIn.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phoebe.Core.Entity
+{
+    using Phoebe.Base.Framework;
+    using SqlSugar;
+
+    /// <summary>
+    /// 入库类
+    /// </summary>
+    public class StockIn : IBaseEntity<string>
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [SugarColumn(ColumnName = "Id", IsPrimaryKey = true)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 入库时间
+        /// </summary>
+        [Display(Name = "入库时间")]
+        public DateTime InTime { get; set; }
+
+        /// <summary>
+        /// 入库月份
+        /// </summary>
+        [Display(Name = "入库月份")]
+        public string MonthTime { get; set; }
+
+        /// <summary>
+        /// 流水单号
+        /// </summary>
+        [Display(Name = "流水单号")]
+        public string FlowNumber { get; set; }
+
+        /// <summary>
+        /// 所属客户
+        /// </summary>
+        [Display(Name = "所属客户")]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// 所属合同
+        /// </summary>
+        [Display(Name = "所属合同")]
+        public int ContractId { get; set; }
+
+        /// <summary>
+        /// 操作人ID
+        /// </summary>
+        [Display(Name = "操作人ID")]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        [Display(Name = "操作人")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Display(Name = "创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 确认时间
+        /// </summary>
+        [Display(Name = "确认时间")]
+        public DateTime? ConfirmTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+        #endregion //Property
+    }
+}
---
+++ Phoebe.Core/Entity/StockInTask.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phoebe.Core.Entity
+{
+    using Phoebe.Base.Framework;
+    using SqlSugar;
+
+    /// <summary>
+    /// 入库任务类
+    /// </summary>
+    public class StockInTask : IBaseEntity<string>
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [SugarColumn(ColumnName = "Id", IsPrimaryKey = true)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 入库ID
+        /// </summary>
+        [Display(Name = "入库ID")]
+        public string StockInId { get; set; }
+
+        /// <summary>
+        /// 库存ID
+        /// </summary>
+        [Display(Name = "库存ID")]
+        public string StoreId { get; set; }
+
+        /// <summary>
+        /// 入库数量
+        /// </summary>
+        [Display(Name = "入库数量")]
+        public int InCount { get; set; }
+
+        /// <summary>
+        /// 入库重量
+        /// </summary>
+        [Display(Name = "入库重量")]
+        public decimal InWeight { get; set; }
+
+        /// <summary>
+        /// 任务码
+        /// </summary>
+        [Display(Name = "任务码")]
+        public string TaskCode { get; set; }
+
+        /// <summary>
+        /// 货架码
+        /// </summary>
+        [Display(Name = "货架码")]
+        public string ShelfCode { get; set; }
+
+        /// <summary>
+        /// 托盘码
+        /// </summary>
+        [Display(Name = "托盘码")]
+        public string TrayCode { get; set; }
+
+        /// <summary>
+        /// 仓位ID
+        /// </summary>
+        [Display(Name = "仓位ID")]
+        public int PositionId { get; set; }
+
+        /// <summary>
+        /// 仓库ID
+        /// </summary>
+        [Display(Name = "仓库ID")]
+        public int WarehouseId { get; set; }
+
+        /// <summary>
+        /// 存放位置
+        /// </summary>
+        [Display(Name = "存放位置")]
+        public string Place { get; set; }
+
+        /// <summary>
+        /// 操作人ID
+        /// </summary>
+        [Display(Name = "操作人ID")]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        [Display(Name = "操作人")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 接单时间
+        /// </summary>
+        [Display(Name = "接单时间")]
+        public DateTime? ReceiveTime { get; set; }
+
+        /// <summary>
+        /// 完成时间
+        /// </summary>
+        [Display(Name = "完成时间")]
+        public DateTime? FinishTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+        #endregion //Method
+    }
+}
