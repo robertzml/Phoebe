f020409ffd8e42359cde843125a31e8e4ef41fbe
robertzml <robertzml@126.com>
2019-12-15 01:52:27 +0800

    add CarryInTaskView.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.Core/BL/View/CarryInTaskViewBusiness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoebe.Core.BL
+{
+    using Phoebe.Base.Framework;
+    using Phoebe.Base.System;
+    using Phoebe.Core.View;
+
+    public class CarryInTaskViewBusiness : AbstractBusiness<CarryInTaskView, string>, IBaseBL<CarryInTaskView, string>
+    {
+        #region Method
+        /// <summary>
+        /// 根据托盘码查找任务
+        /// </summary>
+        /// <param name="trayCode">托盘码</param>
+        /// <param name="status">状态</param>
+        /// <returns></returns>
+        public List<CarryInTaskView> FindByTray(string trayCode, EntityStatus status)
+        {
+            var db = GetInstance();
+
+            var data = db.Queryable<CarryInTaskView>().Where(r => r.TrayCode == trayCode && r.Status == (int)status);
+
+            return data.ToList();           
+        }
+        #endregion //Method
+    }
+}
---
+++ Phoebe.Core/View/CarryInTaskView.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phoebe.Core.View
+{
+    using Phoebe.Base.Framework;
+    using SqlSugar;
+
+    /// <summary>
+    /// 搬运入库任务视图
+    /// </summary>
+    public class CarryInTaskView : IBaseEntity<string>
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [SugarColumn(ColumnName = "Id", IsPrimaryKey = true)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 搬运类型
+        /// </summary>
+        [Display(Name = "搬运类型")]
+        public int Type { get; set; }
+
+        /// <summary>
+        /// 库存ID
+        /// </summary>
+        [Display(Name = "库存ID")]
+        public string StoreId { get; set; }
+
+        /// <summary>
+        /// 入库任务ID
+        /// </summary>
+        [Display(Name = "入库任务ID")]
+        public string StockInTaskId { get; set; }
+
+        /// <summary>
+        /// 出库任务ID
+        /// </summary>
+        [Display(Name = "出库任务ID")]
+        public string StockOutTaskId { get; set; }
+
+        /// <summary>
+        /// 搬运数量
+        /// </summary>
+        [Display(Name = "搬运数量")]
+        public int MoveCount { get; set; }
+
+        /// <summary>
+        /// 搬运重量
+        /// </summary>
+        [Display(Name = "搬运重量")]
+        public decimal MoveWeight { get; set; }
+
+        /// <summary>
+        /// 任务码
+        /// </summary>
+        [Display(Name = "任务码")]
+        public string TaskCode { get; set; }
+
+        /// <summary>
+        /// 货架码
+        /// </summary>
+        [Display(Name = "货架码")]
+        public string ShelfCode { get; set; }
+
+        /// <summary>
+        /// 托盘码
+        /// </summary>
+        [Display(Name = "托盘码")]
+        public string TrayCode { get; set; }
+
+        /// <summary>
+        /// 仓位ID
+        /// </summary>
+        [Display(Name = "仓位ID")]
+        public int PositionId { get; set; }
+
+        /// <summary>
+        /// 仓库ID
+        /// </summary>
+        [Display(Name = "仓库ID")]
+        public int WarehouseId { get; set; }
+
+        /// <summary>
+        /// 存放位置
+        /// </summary>
+        [Display(Name = "存放位置")]
+        public string Place { get; set; }
+
+        /// <summary>
+        /// 清点人ID
+        /// </summary>
+        [Display(Name = "清点人ID")]
+        public int CheckUserId { get; set; }
+
+        /// <summary>
+        /// 清点人
+        /// </summary>
+        [Display(Name = "清点人")]
+        public string CheckUserName { get; set; }
+
+        /// <summary>
+        /// 接单人ID
+        /// </summary>
+        [Display(Name = "接单人ID")]
+        public int ReceiveUserId { get; set; }
+
+        /// <summary>
+        /// 接单人
+        /// </summary>
+        [Display(Name = "接单人")]
+        public string ReceiveUserName { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Display(Name = "创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 清点时间
+        /// </summary>
+        [Display(Name = "清点时间")]
+        public DateTime? CheckTime { get; set; }
+
+        /// <summary>
+        /// 接单时间
+        /// </summary>
+        [Display(Name = "接单时间")]
+        public DateTime? ReceiveTime { get; set; }
+
+        /// <summary>
+        /// 移入时间
+        /// </summary>
+        [Display(Name = "移入时间")]
+        public DateTime? InTime { get; set; }
+
+        /// <summary>
+        /// 完成时间
+        /// </summary>
+        [Display(Name = "完成时间")]
+        public DateTime? FinishTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+
+        /// <summary>
+        /// 入库任务码
+        /// </summary>
+        [Display(Name = "入库任务码")]
+        public string StockInTaskCode { get; set; }
+
+
+        /// <summary>
+        /// 入库数量
+        /// </summary>
+        [Display(Name = "入库数量")]
+        public int InCount { get; set; }
+
+        /// <summary>
+        /// 单位重量
+        /// </summary>
+        [Display(Name = "单位重量")]
+        public decimal UnitWeight { get; set; }
+
+        /// <summary>
+        /// 入库重量
+        /// </summary>
+        [Display(Name = "入库重量")]
+        public decimal InWeight { get; set; }
+
+        /// <summary>
+        /// 规格
+        /// </summary>
+        [Display(Name = "规格")]
+        public string Specification { get; set; }
+
+        /// <summary>
+        /// 产地
+        /// </summary>
+        [Display(Name = "产地")]
+        public string OriginPlace { get; set; }
+
+        /// <summary>
+        /// 保质期
+        /// </summary>
+        [Display(Name = "保质期")]
+        public int Durability { get; set; }
+        #endregion //Property
+    }
+}
