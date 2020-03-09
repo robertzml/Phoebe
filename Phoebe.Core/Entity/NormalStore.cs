dcba7cb529820a259f44e0ec34d6ec361ee3308b
robertzml <robertzml@126.com>
2020-03-09 17:38:38 +0800

    add normal store model.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Phoebe.Core/Entity/NormalStore.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phoebe.Core.Entity
+{
+    using Phoebe.Base.Framework;
+    using SqlSugar;
+
+    /// <summary>
+    /// 普通库存类
+    /// </summary>
+    [SugarTable("NormalStore")]
+    public class NormalStore : IBaseEntity<string>
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [SugarColumn(ColumnName = "Id", IsPrimaryKey = true)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 所属客户
+        /// </summary>
+        [Display(Name = "所属客户")]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// 所属合同
+        /// </summary>
+        [Display(Name = "所属合同")]
+        public int ContractId { get; set; }
+
+        /// <summary>
+        /// 所属货品
+        /// </summary>
+        [Display(Name = "所属货品")]
+        public string CargoId { get; set; }
+
+        /// <summary>
+        /// 仓库ID
+        /// </summary>
+        [Display(Name = "仓库ID")]
+        public int WarehouseId { get; set; }
+
+        /// <summary>
+        /// 存放位置
+        /// </summary>
+        [Display(Name = "存放位置")]
+        public string Place { get; set; }
+
+        /// <summary>
+        /// 在库数量
+        /// </summary>
+        [Display(Name = "在库数量")]
+        public int StoreCount { get; set; }
+
+        /// <summary>
+        /// 在库重量
+        /// </summary>
+        [Display(Name = "在库重量")]
+        public decimal StoreWeight { get; set; }
+
+        /// <summary>
+        /// 批次
+        /// </summary>
+        [Display(Name = "批次")]
+        public string Batch { get; set; }
+
+        /// <summary>
+        /// 产地
+        /// </summary>
+        [Display(Name = "产地")]
+        public string OriginPlace { get; set; }
+
+        /// <summary>
+        /// 保质期
+        /// </summary>
+        [Display(Name = "保质期")]
+        public int Durability { get; set; }
+
+        /// <summary>
+        /// 入库时间
+        /// </summary>
+        [Display(Name = "入库时间")]
+        public DateTime InTime { get; set; }
+
+        /// <summary>
+        /// 出库时间
+        /// </summary>
+        [Display(Name = "出库时间")]
+        public DateTime? OutTime { get; set; }
+
+        /// <summary>
+        /// 入库任务单ID
+        /// </summary>
+        [Display(Name = "入库任务单ID")]
+        public string StockInTaskId { get; set; }
+
+        /// <summary>
+        /// 出库任务单ID
+        /// </summary>
+        [Display(Name = "出库任务单ID")]
+        public string StockOutTaskId { get; set; }
+
+        /// <summary>
+        /// 前序库存ID
+        /// </summary>
+        [Display(Name = "前序库存ID")]
+        public string PrevStoreId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Display(Name = "创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+        #endregion //Property
+    }
+}
