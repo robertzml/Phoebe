bbd7c38402b4570bf7702a169f4c07d2778829e6
robertzml <robertzml@126.com>
2021-05-29 21:51:58 +0800

    add IceSale.

---
+++ Phoebe.Core/BL/IceSaleBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoebe.Core.BL
+{
+    using SqlSugar;
+    using Phoebe.Base.Framework;
+    using Phoebe.Base.System;
+    using Phoebe.Core.Entity;
+    using Phoebe.Core.Utility;
+
+    /// <summary>
+    /// 冰块销售业务类
+    /// </summary>
+    public class IceSaleBusiness : AbstractBusiness<IceSale, string>, IBaseBL<IceSale, string>
+    {
+    }
+}
---
+++ Phoebe.Core/Entity/IceSale.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Phoebe.Core.Entity
+{
+    using Phoebe.Base.Framework;
+    using SqlSugar;
+
+    /// <summary>
+    /// 冰块销售类
+    /// </summary>
+    [SugarTable("IceSale")]
+    public class IceSale : IBaseEntity<string>
+    {
+        #region Property
+        /// <summary>
+        /// ID
+        /// </summary>
+        [SugarColumn(ColumnName = "Id", IsPrimaryKey = true)]
+        [Display(Name = "ID")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 销售时间
+        /// </summary>
+        [Required]
+        [Display(Name = "销售时间")]
+        public DateTime SaleTime { get; set; }
+
+        /// <summary>
+        /// 流水单号
+        /// </summary>
+        [Display(Name = "流水单号")]
+        public string FlowNumber { get; set; }
+
+        /// <summary>
+        /// 所属客户
+        /// </summary>
+        [Display(Name = "所属客户")]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// 所属合同
+        /// </summary>
+        [Display(Name = "所属合同")]
+        public int ContractId { get; set; }
+
+        /// <summary>
+        /// 冰块类型
+        /// </summary>
+        [Required]
+        [Display(Name = "冰块类型")]
+        public int IceType { get; set; }
+
+        /// <summary>
+        /// 销售数量
+        /// </summary>
+        [Display(Name = "销售数量")]
+        public int SaleCount { get; set; }
+
+        /// <summary>
+        /// 销售单价
+        /// </summary>
+        [Display(Name = "销售单价")]
+        public decimal SaleUnitPrice { get; set; }
+
+        /// <summary>
+        /// 销售金额
+        /// </summary>
+        [Display(Name = "销售金额")]
+        public decimal SaleFee { get; set; }
+
+        /// <summary>
+        /// 操作人ID
+        /// </summary>
+        [Display(Name = "操作人ID")]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        [Display(Name = "操作人")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Display(Name = "创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [Display(Name = "备注")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+        #endregion //Property
+    }
+}
